444495c8c860a0656cb50c0dbeca85fe6bedc53a
玖叁 <colour_93@furry.top>
2022-11-10 16:55:38 +0800

    🎉 初次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+/**
+ * VuePress 配置文件
+ */
+
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: 'FurBot-API',
+  description: '服务于 Furry 文化主题 QQ 社群 Bot 的公共应用接口项目。',
+})
